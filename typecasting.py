3277cc4b7769ec0438013e2d7f0243adaa68f0e5
rithwik777222 <rithwikk312@gmail.com>
2025-03-18 18:55:46 +0530

    first commit

---
+++ typecasting.py
@@ -0,0 +1,18 @@
+name = "penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+print("name :", name)
+print("data type of name is:", type(name))
+print("age:", age)
+print("data type of age", type(age))
+print("is_student :", is_student)
+print("data type of is_student is", type(is_student))
+print("weight:", weight)
+print("data type of weight is", type(weight))
+
+print("\n after type casting....")
+age = str(age)
+print(age)
+print("data type of age is",type(age))
