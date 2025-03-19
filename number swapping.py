b48a88ba55512ced670c5675543e2cab0da81fef
rithwik777222 <rithwikk312@gmail.com>
2025-03-19 17:50:00 +0530

    number swapping

---
+++ number swapping.py
@@ -0,0 +1,12 @@
+#taking input value from user
+x = int(input("value of x:"))
+y = int(input("value of y:"))
+
+#swapping
+temp = x 
+x = y
+y = temp
+
+#displaying results after swapping
+print("the value of x after swapping is:",x)
+print("the value of y after swapping is:",y)
